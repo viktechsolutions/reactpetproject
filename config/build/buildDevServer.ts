d19e0fdccaf7bef474e6b3d1ba3af7db283093f3
viktechsolutions <viktechsolutions@yandex.ru>
2023-11-26 20:35:51 +0300

    initial commit

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,10 @@
+import type {Configuration as WebpackConfiguration} from "webpack-dev-server";
+import {BuildOptions} from "./types/config";
+
+export function buildDevServer(options: BuildOptions): WebpackConfiguration {
+    return {
+         port: options.port,
+         open: true,
+         historyApiFallback: true
+    }
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,35 @@
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import path from "path";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {mode, paths, isDev} = options;
+
+    return {
+        mode,
+        entry:
+            paths.entry,
+        output:
+            {
+                path: paths.build,
+                filename:
+                    '[name].[contenthash].js',
+                clean:
+                    true
+            }
+        ,
+        plugins: buildPlugins(options),
+        module:
+            {
+                rules: buildLoaders(options)
+            }
+        ,
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer:isDev ? buildDevServer(options) : undefined
+    }
+}
