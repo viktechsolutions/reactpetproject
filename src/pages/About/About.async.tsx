b19f1b1b8d2a4aaf75311d28d1ce79580aa01831
viktechsolutions <viktechsolutions@yandex.ru>
2023-11-26 20:35:41 +0300

    initial commit

---
+++ src/pages/About/About.async.tsx
@@ -0,0 +1,4 @@
+import { lazy} from "react";
+
+// @ts-ignore
+export const AboutAsync = lazy(() => import('./About'));
---
+++ src/pages/Main/Main.async.tsx
@@ -0,0 +1,4 @@
+import {lazy}   from "react";
+
+// @ts-ignore
+export const MainAsync = lazy(() => import('./Main'));
